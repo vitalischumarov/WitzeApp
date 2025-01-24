2fefad35f80e3526871a26c98148e2d0ae377644
Vitali Schumarov <v.schumarov@icloud.com>
2025-01-24 05:31:00 +0100

    changed saveBtn display

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 import { fetchData } from "./api";
 
-let newJoke = "";
+let firstJokeLoaded = false;
 
 async function loadNewJoke() {
   let joke = await fetchData();
   displayNewJoke(joke);
+  displaySaveJokeButton();
 }
 
 function displayNewJoke(nextJoke) {
-  let joke = document.querySelector(".jokeBox__joke");
-  joke.innerHTML = nextJoke;
+  let previousJoke = document.querySelector(".jokeBox__joke");
+  previousJoke.innerHTML = nextJoke;
+}
+
+function displaySaveJokeButton() {
+  if (firstJokeLoaded) {
+    return;
+  }
+  console.log(firstJokeLoaded);
+  firstJokeLoaded = true;
+  console.log(firstJokeLoaded);
+  let saveButton = document.querySelector(".buttons__saveBtn");
+  console.log(saveButton);
+  saveButton.style.display = "block";
+  // saveButton.setAttribute('display','block')
 }
 
 // Event Listener
